7c775a70f4819517b437f721d0395aed59320796
Nathaniel Hobbs <hobbs.nathaniel@gmail.com>
2016-08-13 18:12:02 +0800

    added code for blue led to denote connection to wifi via heartbeat

--- WiFiClient_moisture_sensor.ino
+++ WiFiClient_moisture_sensor.ino
@@ -7,8 +7,19 @@
 #include <ESP8266WiFiMulti.h>
 #include "settings.h"
 
+//map digital pin numbers to words
+#define GREEN1 0
+#define GREEN2 2
+#define GREEN3 4
+#define YELLOW 5
+#define RED 12
+#define BLUE 13
+#define RELAY 16
+
 ESP8266WiFiMulti WiFiMulti;
+WiFiClient client; // Use WiFiClient class to create TCP connections
 int led_state = 0;
+const int SENSOR_MAX = 1023;
 
 void setup() {
   Serial.begin(115200);  //connect to serial at 115200 bits/sec (baud=signal changes/sec)
@@ -19,12 +30,12 @@ void setup() {
   WiFiMulti.addAP(WIFI_SSID, WIFI_PSWD);   //home
 
    // set LEDs
-  pinMode(0, OUTPUT); //green1
-  pinMode(2, OUTPUT); //green2
-  pinMode(4, OUTPUT); //green3
-  pinMode(5, OUTPUT); //yellow
-  pinMode(12, OUTPUT); //red
-
+  pinMode(GREEN1, OUTPUT); //green1
+  pinMode(GREEN2, OUTPUT); //green2
+  pinMode(GREEN3, OUTPUT); //green3
+  pinMode(YELLOW, OUTPUT); //yellow
+  pinMode(RED, OUTPUT); //red
+  pinMode(BLUE, OUTPUT);//blue
 
   Serial.println();
   Serial.println();
@@ -47,76 +58,17 @@ void setup() {
 void loop() {
   const uint16_t port = HOST_PORT;
   const char *host = HOST_IP; // ip or dns
-  int sensorValue = analogRead(A0);
+  int sensor_value = analogRead(A0);
   
   // Print log to serial
   Serial.print("connecting to ");
   Serial.println(host);
   Serial.print("sensor reading: ");
-  Serial.println(sensorValue);
+  Serial.println(sensor_value);
 
-  // after adding water: 330  | 600..700   | 530..550 |  680  | 375..590 | 
-  // before adding water: ??? | 1018..1024 | 960..970 | 958-9 |   1024   | 
-
-  if(sensorValue > 800){
-    digitalWrite(0, HIGH);
-    digitalWrite(2, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(12, HIGH); 
-    
-    Serial.println("level 5");
-  }
-  else if(sensorValue > 600 && sensorValue < 800){
-    digitalWrite(0, HIGH);
-    digitalWrite(2, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, HIGH);
-    digitalWrite(12, LOW);
-    
-    Serial.println("level 4");
-  }
-  else if(sensorValue > 400 && sensorValue < 600){
-    digitalWrite(0, HIGH);
-    digitalWrite(2, HIGH);
-    digitalWrite(4, HIGH);
-    digitalWrite(5, LOW);
-    digitalWrite(12, LOW);
-    
-    Serial.println("level 3");
-  }
-  else if(sensorValue > 230 && sensorValue < 400){
-    digitalWrite(0, HIGH);
-    digitalWrite(2, HIGH);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(12, LOW);
-    
-    Serial.println("level 2");
-  }
-  else if(sensorValue > 175 && sensorValue < 230){
-    digitalWrite(0, HIGH);
-    digitalWrite(2, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(12, LOW);
-    
-    Serial.println("level 1");
-  }
-  else{
-    digitalWrite(0, LOW);
-    digitalWrite(2, LOW);
-    digitalWrite(4, LOW);
-    digitalWrite(5, LOW);
-    digitalWrite(12, LOW);
-    
-    Serial.println("level 0");
-  }
-
-
+  handleLights(sensor_value);
+  handlePump(sensor_value);
   
-  // Use WiFiClient class to create TCP connections
-  WiFiClient client;
   // try to establish tcp connection
   if (!client.connect(host, port)) {
       Serial.println("connection failed");
@@ -126,12 +78,87 @@ void loop() {
   }
 
   // Send LED heartbeat request to the server
+  heartBeat(led_state);
+
+  
+  // POST moisture value to server
+  postSensorValue(sensor_value);
+
+  Serial.println("closing connection");
+  client.stop();
+  
+  Serial.println("wait 2 sec...");
+  delay(2000);
+}
+
+void handlePump(int sensor_value){
+  if(between(sensor_value, 800, SENSOR_MAX)){
+    digitalWrite(RELAY, LOW); //Turns ON Relay
+    Serial.println("Pump ON");
+    delay(5000); //wait 5 seconds
+    digitalWrite(RELAY, HIGH); //Turns OFF Relay
+    Serial.println("Pump OFF");
+  }
+}
+
+void handleLights(int sensor_value){
+  // after adding water: 330  | 600..700   | 530..550 |  680  | 375..590 | 
+  // before adding water: ??? | 1018..1024 | 960..970 | 958-9 |   1024   | 
+
+  if(between(sensor_value, 800, SENSOR_MAX)){
+    digitalWrite(RED, HIGH); 
+    
+    Serial.println("level 5");
+  }
+  else if(between(sensor_value, 600, 800)){
+    digitalWrite(YELLOW, HIGH);
+    digitalWrite(RED, LOW);
+    
+    Serial.println("level 4");
+  }
+  else if(between(sensor_value, 400, 600)){
+    digitalWrite(GREEN3, HIGH);
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(RED, LOW);
+    
+    Serial.println("level 3");
+  }
+  else if(between(sensor_value, 230, 400)){
+    digitalWrite(GREEN2, HIGH);
+    digitalWrite(GREEN3, LOW);
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(RED, LOW);
+    
+    Serial.println("level 2");
+  }
+  else if(between(sensor_value, 175, 230)){
+    digitalWrite(GREEN1, HIGH);
+    digitalWrite(GREEN2, LOW);
+    digitalWrite(GREEN3, LOW);
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(RED, LOW);
+    
+    Serial.println("level 1");
+  }
+  else{
+    digitalWrite(GREEN1, LOW);
+    digitalWrite(GREEN2, LOW);
+    digitalWrite(GREEN3, LOW);
+    digitalWrite(YELLOW, LOW);
+    digitalWrite(RED, LOW);
+    
+    Serial.println("level 0");
+  }
+}
+
+void heartBeat(int led_state){
   client.println("POST /light HTTP/1.1");
   client.println("Content-Type: application/x-www-form-urlencoded");
   client.println("Content-Length: 7");
   client.println("");//need empty line before body
   if(led_state == 0){
     Serial.println("light on");
+    digitalWrite(BLUE, HIGH);
     
     // send POST to server turning on light
     client.println("state=1"); 
@@ -140,30 +167,29 @@ void loop() {
     led_state = 1;
   }else{
     Serial.println("light off");
+    digitalWrite(BLUE,LOW);
 
     // send POST to server turning off light
     client.println("state=0");
     
      led_state = 0;
   }
+}
 
-  
-  // POST moisture value to server
+void postSensorValue(int sensor_value){
   client.println("POST /plants HTTP/1.1");
   client.println("Content-Type: application/x-www-form-urlencoded");
   client.println("Content-Length: 20");  //16 chars and up to 4 digits
   client.println(""); //need empty line before body
   client.print("name=1&moisture=");
-  client.println(sensorValue);
+  client.println(sensor_value);
 
   //read back one line from server
   String line = client.readStringUntil('\r');
   client.println(line);
+}
 
-  Serial.println("closing connection");
-  client.stop();
-  
-  Serial.println("wait 2 sec...");
-  delay(2000);
+boolean between(int value, int min, int max){
+  return value >= min && value <=max;
 }
 
