e1ff1736a9b92c73cf092533212b976bedcc38e4
Nathaniel Hobbs <hobbs.nathaniel@gmail.com>
2017-01-12 15:48:17 +0800

    added default settings file to be modified by user for connecting to wifi/reporting to server

---
+++ default.settings.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+// Note no semicolon after a hash define
+#define WIFI_SSID "name_of_wifi_network"        //ssid of wifi network espduino should connect to
+#define WIFI_PSWD "wifi_network_password"	//password of above wifi network
+#define HOST_IP "address_of_monitor_server" 	//ip address of server to send sensor info to
+#define HOST_PORT 80				//port server is listening on
+
+
+#endif
